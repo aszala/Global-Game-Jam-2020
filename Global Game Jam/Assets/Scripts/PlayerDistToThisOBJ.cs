b3227306da0fb26f534e4de364736d977bed9b58
Abhay Zala <zala.abhay@gmail.com>
2020-02-01 11:45:29 -0500

    Added distance script

--- Global Game Jam/Assets/Scripts/PlayerDistToThisOBJ.cs
+++ Global Game Jam/Assets/Scripts/PlayerDistToThisOBJ.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerDistToThisOBJ : MonoBehaviour {
+    
+    private GameObject player;
+    
+    public UnityEvent onPlayerReach;
+
     void Start() {
-        
+        player = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     void Update() {
-        
+        if (Vector3.Distance(player.transform.position, transform.position) < 0.5f) {
+            onPlayerReach.Invoke();
+
+            Destroy(this.gameObject);
+        }
     }
 }
