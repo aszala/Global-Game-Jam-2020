8f0b3a46ca80f3a8fa91b33b1bff69c2b114875a
davidlu51412 <55219650+davidlu51412@users.noreply.github.com>
2020-02-01 11:56:16 -0500

    bob

---
+++ Global Game Jam/Assets/Scripts/ArrowBob.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBob : MonoBehaviour
+{
+    private Transform arrow;
+    [SerializeField] private float oscillation;
+    [SerializeField] private float oscillationSpeed;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        arrow = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        arrow.Translate(new Vector3(0,oscillation * Mathf.Cos(Time.time * oscillationSpeed), 0));
+        arrow.Rotate(new Vector3(0, 1, 0));
+
+
+
+    }
+}
