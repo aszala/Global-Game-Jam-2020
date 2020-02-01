bd4ff27b224dafa0c94879484d3ce77d149d36e7
Abhay Zala <zala.abhay@gmail.com>
2020-02-01 13:02:12 -0500

    Kinda made sound

--- Global Game Jam/Assets/Scripts/Tutorial.cs
+++ Global Game Jam/Assets/Scripts/Tutorial.cs
@@ -3,15 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tutorial : MonoBehaviour {
-    void Start() {
-        
-    }
 
-    void Update() {
+    private AudioSource audioSource;
+
+    public AudioClip[] audioClips;
+
+    private int currentInstruction = 0, timeTillNextLine = 1500;
+
+    private int droneCount = 0, deadDrones = 0;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
         
+        StartCoroutine(playClip(currentInstruction));
+        StartCoroutine(playClip(++currentInstruction));
+        StartCoroutine(playClip(++currentInstruction));
     }
 
     public void onPlayerReachPointA() {
-        print("Yeet");
+        StartCoroutine(playClip(++currentInstruction));
+    }
+
+    public void setDroneCount(int drones) {
+        droneCount = drones;
+        deadDrones = 0;
+    }
+
+    public void droneDeath() {
+        deadDrones++;
+
+        if (deadDrones == droneCount) {
+            StartCoroutine(playClip(++currentInstruction));
+        }
+    }
+
+    public IEnumerator playClip(int index) {
+        yield return new WaitForSeconds(timeTillNextLine);
+
+        
+
+        if (audioSource.isPlaying) {
+            audioSource.Stop();
+        }
+
+        audioSource.clip = audioClips[index];
+
+        audioSource.Play();
+
+        yield return new WaitForSeconds(audioClips[index].length);
     }
 }
