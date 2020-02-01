ac5d296bf4af4ff258c5ae564f35bf5e749885f0
davidlu51412 <55219650+davidlu51412@users.noreply.github.com>
2020-02-01 14:51:51 -0500

    test

--- Global Game Jam/Assets/Scripts/Tutorial.cs
+++ Global Game Jam/Assets/Scripts/Tutorial.cs
@@ -72,18 +72,67 @@ public class Tutorial : MonoBehaviour
         audioSource.clip = audioClips[index];
 
         audioSource.Play();
-        currentInstruction++;
+        if (currentInstruction == 0)
+        {
+            StartCoroutine(playClip(currentInstruction));
+            currentInstruction++;
 
-		if (currentInstruction == 3) {
-			tutorialLocation.SetActive(true);
-		} else if (currentInstruction == 4) {
+        }
+        else if (currentInstruction == 1)
+        {
+            StartCoroutine(playClip(currentInstruction));
+            currentInstruction++;
+        }
+        else if (currentInstruction == 2)
+        {
+            tutorialLocation.SetActive(true);
+
+        }
+        else if (currentInstruction == 3)
+        {
+             StartCoroutine(playClip(currentInstruction));
+            currentInstruction++;
+        }
+        else if (currentInstruction == 4)
+        {
+           
+
+        }
+        else if (currentInstruction == 5)
+        {
             setDroneCount(1);
             Instantiate(drone);
-
-            StartCoroutine(playClip(currentInstruction));
-        } else {
-            StartCoroutine(playClip(currentInstruction));
         }
+        else if (currentInstruction == 6)
+        {
+            StartCoroutine(playClip(currentInstruction));
+            currentInstruction++;
+        }
+        else if (currentInstruction == 7)
+        {
+
+        }
+        else if (currentInstruction == 8)
+        {
+
+        }
+        else if (currentInstruction == 9)
+        {
+            StartCoroutine(playClip(currentInstruction));
+            currentInstruction++;
+        }  else if (currentInstruction == 10)
+        {
+
+        }
+         else if (currentInstruction == 11)
+        {
+
+        }
+         else if (currentInstruction == 12)
+        {
+
+        }
+    
     }
 
 }
