e766a943c4a25d128a1c73f4f5b740654b199bfa
Abhay Zala <zala.abhay@gmail.com>
2020-02-01 15:44:27 -0500

    Drone AI

---
+++ Global Game Jam/Assets/Scripts/DroneAITutorial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneAI : MonoBehaviour {
+
+    public GameObject player;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        transform.LookAt(player.transform);
+
+        if (Vector3.Distance(transform.position, player.transform.position) > 5f) {
+            transform.position += 5f * transform.forward * Time.deltaTime;
+        }
+    }
+}
