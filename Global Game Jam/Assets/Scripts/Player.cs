83054d7a7aa50e40017d6ae8a0528fa72ab8bff5
davidlu51412 <55219650+davidlu51412@users.noreply.github.com>
2020-02-01 12:24:08 -0500

    health

---
+++ Global Game Jam/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health;
+    private int maxhealth = health;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateHealth(int deltaHealth) {
+        if (health + deltaHealth <= maxhealth) {
+  health += deltaHealth;
+        }
+      
+        print(health);
+    }
+
+}
